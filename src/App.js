13701a9781b2a0e2e9f8a881a548276aa396e484
Richardsonn1 <marcus.richardson@hotmail.se>
2021-09-06 16:09:34 +0200

    updated css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import "./App.css"
+import "./App.css";
+import sun from '../src/images/icon-sun.svg';
 
 export default class App extends Component {
   state = { time: new Date() };
@@ -20,8 +21,17 @@ export default class App extends Component {
     return (
       <>
         <div className="main">
+          <div className="quote">
+            <p>"The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value."</p>
+            <h4>Ada Lovelace</h4>
+          </div>
           <div className="daylight">
+            <div className="currently-container">
+              <img src={sun} alt="icon" />
+              <p>GOOD MORNING, IT'S CURRENTLY</p>
+            </div>
             <h1> {this.state.time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} </h1>
+            <p className="location">IN GOTHENBURG, SWE</p>
           </div>
         </div>
       </>
